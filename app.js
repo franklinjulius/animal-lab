025280902db9962f73348ec73140a18aa115d043
Julius Franklin <franklinjulius88@gmail.com>
2021-07-15 21:17:35 -0400

    new animal lab

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-// import functions and grab DOM elements
+const h1 = document.getElementById('animal-name');
+const horsePic = document.getElementById('horse-picture');
+const catPic = document.getElementById('cat-picture');
+const dogPic = document.getElementById('dog-picture');
+const horseSound = document.getElementById('horse-sound');
+const catSound = document.getElementById('cat-sound');
+const dogSound = document.getElementById('dog-sound');
 
-// initialize state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+horseSound.addEventListener('click', () => {
+  horseSound.play();
+});
+
+catSound.addEventListener('click', () => {
+  catSound.play();
+});
+
+dogSound.addEventListener('click', () => {
+  dogSound.play();
+});
